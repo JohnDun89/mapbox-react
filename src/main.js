855b56f148a5fa18bed42bc812008ae57e070466
John Duncan <john@johnduncanphoto.com>
2018-06-18 10:45:47 +0100

    stable

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import MapContainer from "./mapContainer.js";
-import ParticleSystem from "./particleSystem.js";
 class Main extends Component {
   render() {
     return (
       <div className="Main">
         <MapContainer />
-        <ParticleSystem />
       </div>
     );
   }
